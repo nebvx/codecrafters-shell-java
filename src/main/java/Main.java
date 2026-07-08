c9894ff4204fe84d1253640c88d1eb0ddd6dc8ad
nebvx <berndorfer.lena@gmail.com>
2026-07-08 20:28:29 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
-        Scanner scanner = new Scanner(System.in);
-        String userInput = scanner.nextLine();
-        System.out.println(userInput + ": command not found");
+        REPL shellLoop = new REPL();
+        shellLoop.runLoop();
     }
 }
---
+++ src/main/java/REPL.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class REPL {
+    Scanner scanner;
+
+    // Read-Eval-Print Loop
+    public REPL() {
+
+    }
+
+    public void runLoop() {
+        while (true) {
+            System.out.print("$ ");
+            scanner = new Scanner(System.in);
+            String userInput = scanner.nextLine();
+            System.out.println(userInput + ": command not found");
+        }
+    }
+
+}
