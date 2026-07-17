42a2f386054012b2c35bfa1b4e8a458af88ec632
nebvx <berndorfer.lena@gmail.com>
2026-07-17 16:47:17 +0200

    codecrafters submit [skip ci]

--- src/main/java/command/Type.java
+++ src/main/java/command/Type.java
@@ -7,16 +7,25 @@ public class Type {
     public Type(String command) {
         this.command = command;
     }
-
+    //Files.isExecutable()
     public void run() {
-        if (isCommandinBuiltin()) {
+        if (isCommandInBuiltin()) {
             System.out.println(command + " is a shell builtin");
+
+        } else if(isCommandInPath()) {
+
         } else {
             System.out.println(command + ": not found");
         }
     }
 
-    private boolean isCommandinBuiltin() {
+    private boolean isCommandInPath() {
+
+
+        return false;
+    }
+
+    private boolean isCommandInBuiltin() {
         for (Builtin builtin : Builtin.values()) {
             if (builtin.name().equals(command)) {
                 return true;
