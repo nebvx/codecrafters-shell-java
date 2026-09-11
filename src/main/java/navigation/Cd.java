ea43fc92788ec93570fb86dada10b9f6c8d636ed
nebvx <berndorfer.lena@gmail.com>
2026-09-11 20:51:35 +0200

    codecrafters submit [skip ci]

--- src/main/java/navigation/Cd.java
+++ src/main/java/navigation/Cd.java
@@ -10,17 +10,42 @@ import static util.FindPath.findInPath;
 
 public class Cd {
     String commandPath;
+    Path path;
 
     public Cd(String commandPath) {
         this.commandPath = commandPath;
+        path = Paths.get(commandPath);
     }
 
     public void run() {
-        Path path =  Paths.get(commandPath);
-        if (Files.exists(path) && Files.isDirectory(path)) {
-            System.setProperty("user.dir", commandPath);
-        } else {
-            System.out.println("cd: " + path + ": No such file or directory");
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            System.err.println("cd: " + path + ": No such file or directory");
         }
+
+        if(commandPath.charAt(0) == '/') { //absolute path
+            cdWithAbsolutePath();
+        } else if(commandPath.charAt(0) == '.') {
+            if (commandPath.charAt(1) == '/') { //Current Directory
+                cdWithCurrentDirectory();
+            } else if (commandPath.charAt(1) == '.') { //Parent Directory
+                cdWithParentDirectory();
+            }
+        } else { //is any letter
+            cdWithCurrentDirectory();
+        }
+    }
+
+    private void cdWithAbsolutePath() {
+        System.setProperty("user.dir", commandPath);
+    }
+
+    private void cdWithCurrentDirectory() {
+        Path absolutePath = Paths.get(System.getProperty("user.dir"), commandPath).toAbsolutePath().normalize();
+        System.setProperty("user.dir", absolutePath.toString());
+    }
+
+    private void cdWithParentDirectory() {
+        Path absolutePath = Paths.get(System.getProperty("user.dir"), commandPath).toAbsolutePath().normalize();
+        System.setProperty("user.dir", absolutePath.toString());
     }
 }
